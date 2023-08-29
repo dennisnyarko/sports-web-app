b4071c084e62bf0c4e1f27b568fb635a04cdc58a
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-08-29 21:46:10 +0000

    add arrow

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,3 +1,4 @@
+import { arrowRight } from "../assets/icons"
 import { offer } from "../assets/images"
 import Button from "../components/Button"
 
@@ -16,8 +17,8 @@ const SpecialOffer = () => {
          From premier selections to incredible savings, we offer unparalleled value that sets us apart</p>
         <p className="mt-6 lg:max-w-lg info-text">Navigate a realm of possibilities designed to fulfill your unique desires, surpassing
          the loftiest expectations. Your journey with us is nothing short of exceptional</p>
-        <div className="mt-11">
-          <Button label="View details" />
+        <div className="mt-11 flex flex-wrap gap-4">
+          <Button label="Shop now" iconURL={arrowRight} />
           <Button label="Learn more" borderColor="border-slate-gray" textColor="text-slate-gray"/>
         </div>
       </div>
